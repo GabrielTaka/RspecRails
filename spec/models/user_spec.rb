468d2901cf6e6b839fab984840bd18f8617e4183
Gabriel Vargas <gabriel.vargas@continuodev.com.br>
2019-07-16 12:03:50 -0300

    teste para validação de email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,17 @@ describe User do
     expect(user).to be_valid
   end
 
-  it 'não é válido quando o primeiro nome é nulo '
+  it 'não é válido quando o primeiro nome é nulo ' do
+    user = User.new(firstname: nil)
+    user.valid?
+    expect(user.errors[:firstname]).to include("can't be blank")
+  end
 
+  it "é invalido caso já exista um e-mail igual" do
+    user = User.create(firstname: 'Gabriel', lastname: 'Takahashi', email: 'gabriel.vargas01@fatec.sp.gov.br')
+    user = User.create(firstname: 'Gabriel', lastname: 'vargas', email: 'gabriel.vargas01@fatec.sp.gov.br')
+
+    user.valid?
+    expect(user.errors[:email]).not_to include("has already been taken")
+  end
 end
