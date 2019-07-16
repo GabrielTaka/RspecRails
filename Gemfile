9988111982cda87dc42d63e89414b789b9c818af
Gabriel Vargas <gabriel.vargas@continuodev.com.br>
2019-07-16 11:48:13 -0300

    criando teste unitários

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
-gem "rspec-rails", "~> 3.1.0"
+gem 'rspec-rails', '~> 3.5.0.beta'
 
 gem 'rake', '< 11.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
+extend ActiveSupport::Concern
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+describe User do
+  it 'é válido quando contém o primeiro nome, último nome e email.' do
+    user = User.new(
+      firstname: 'Gabriel',
+      lastname: 'Vargas',
+      email: 'gabriel.vargas01@fatec.sp.gov.br'
+    )
+
+    expect(user).to be_valid
+  end
+
+  it 'não é válido quando o primeiro nome é nulo '
+
 end
